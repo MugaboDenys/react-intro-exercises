efb8c59c7d5b3fbb1bc5bbdd107b0ec5b3058071
MugaboDenys <mugabodan2020@gmail.com>
2023-03-22 17:42:47 +0200

    custom components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import { useRef } from 'react'
 import './App.css'
+import Button from './components/Button'
 
 function App() {
 
-  const handleClick = () =>{
-    window.alert("Clicked!")
-  }
+  const buttonRef = useRef(null)
 
+  const handleClick = () =>{
+    window.alert(`You clicked ${buttonRef.current.textContent}`)
+  }
   const genLinks = () =>{
     let arr = []
     for(let i = 0; i< 10; i++){
@@ -22,7 +25,17 @@ function App() {
           <a key={index} href={item.link} className={`px-2 underline underline-offset-4 ${!index == 0 && "border-indigo-600 border-l"} text-indigo-600 `} >{index == 0 ? "Home" : item.text}</a>
         ))}
       </div>
-      <button onClick={handleClick} className="py-2 px-4 border-2 mt-64 border-black rounded">Click Me</button>
+      <div className='flex gap-10'>
+        <Button >
+          Button 1
+        </Button>
+        <Button >
+          Button 2
+        </Button>
+        <Button >
+          Button 3
+        </Button>
+      </div>
     </div>
   )
 }
---
+++ src/components/Button.jsx
@@ -0,0 +1,16 @@
+import { useRef } from "react";
+
+const Button = ({children}) => {
+    const buttonRef = useRef(null);
+
+  function handleClick() {
+    window.alert(`You clicked ${buttonRef.current.textContent}`);
+  }
+    return ( 
+        <button ref={buttonRef} onClick={handleClick} className="py-2 px-4 border-2 mt-64 border-black rounded">
+            {children}
+        </button>
+     );
+}
+ 
+export default Button;
