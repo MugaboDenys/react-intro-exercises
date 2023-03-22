96bb427967b191beb5f3ce5c54198b934459eda0
MugaboDenys <mugabodan2020@gmail.com>
2023-03-22 13:49:46 +0200

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
 
   return (
-    <div className="h-screen flex items-center justify-center bg-yellow-500">
-      <h1 className="text-3xl font-light ">Hello, world!</h1>
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
