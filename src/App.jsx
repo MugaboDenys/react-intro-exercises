16d813093557bd360a12c46555cfbcc4dcb80a85
MugaboDenys <mugabodan2020@gmail.com>
2023-03-22 17:06:10 +0200

    added a navbar

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,23 @@ function App() {
     window.alert("Clicked!")
   }
 
+  const genLinks = () =>{
+    let arr = []
+    for(let i = 0; i< 10; i++){
+      arr.push({text : `Exercise ${i}`, link: `/exercise${i}`})
+    }
+    return arr;
+  }
+
+  const array = genLinks()
   return (
-    <div className="h-screen flex items-center justify-center">
-      <button onClick={handleClick} className="py-2 px-4 border-2 border-black rounded">Click Me</button>
+    <div className="h-screen flex flex-col items-center ">
+      <div>
+        {array.map((item, index)=>(
+          <a key={index} href={item.link} className={`px-2 underline underline-offset-4 ${!index == 0 && "border-indigo-600 border-l"} text-indigo-600 `} >{index == 0 ? "Home" : item.text}</a>
+        ))}
+      </div>
+      <button onClick={handleClick} className="py-2 px-4 border-2 mt-64 border-black rounded">Click Me</button>
     </div>
   )
 }
