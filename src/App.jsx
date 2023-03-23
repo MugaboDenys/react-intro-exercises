30268d66394320c86a808a3b977173f65ade0e32
MugaboDenys <mugabodan2020@gmail.com>
2023-03-23 13:14:44 +0200

    Mapping array with component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Item from './components/Item';
 
 function App() {
 
@@ -11,7 +12,7 @@ function App() {
   }
 
   const array = genLinks()
-  const array1 = ['dog', 'cat', 'chicken', 'cow', 'sheep', 'horse']
+  const array1 = ['🐶 dog', '😾 cat', '🐤 chicken', '🐮 cow', '🐑 sheep', '🐴 horse']
   return (
     <div className="h-screen flex flex-col items-center ">
       <div>
@@ -19,9 +20,9 @@ function App() {
           <a key={index} href={item.link} className={`px-2 underline underline-offset-4 ${!index == 0 && "border-indigo-600 border-l"} text-indigo-600 `} >{index == 0 ? "Home" : item.text}</a>
         ))}
       </div>
-      <ul className='mt-32 flex flex-col items-start'>
+      <ul className='mt-32 rounded-md w-52 border-2 justify-center flex flex-col items-start'>
           {array1.map(item=>(
-            <li key={item} className="list-disc">{item}</li>
+            <Item key={item}>{item}</Item>
           ))}
       </ul>
     </div>
---
+++ src/components/Item.jsx
@@ -0,0 +1,7 @@
+const Item = ({children}) => {
+    return ( 
+        <li className="border-b w-full py-5">{children}</li>
+     );
+}
+ 
+export default Item;
