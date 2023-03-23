0e7de8c32341d24f34a0e3dfe0eec45a2fdcad42
MugaboDenys <mugabodan2020@gmail.com>
2023-03-23 17:44:11 +0200

    Rendering json data

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
+import Card from "./components/Card"
+import data from "./data"
+
 const App = () => {
 
   const handleClick = () => window.alert("Clicked!")
+
 
   const genLinks = () =>{
     let arr = []
@@ -18,7 +22,11 @@ const App = () => {
           <a key={index} href={item.link} className={`px-2 underline underline-offset-4 ${!index == 0 && "border-indigo-600 border-l"} text-indigo-600 `} >{index == 0 ? "Home" : item.text}</a>
         ))}
       </div>
-      <button onClick={handleClick} className="py-2 px-4 border-2 mt-64 border-black rounded">Click Me</button>
+      <div className="w-[40rem] p-5 h-72 mt-20 bg-[#dff8db] flex gap-5 items-center rounded-md">
+        {data.map(item=>(
+          <Card key={item.id} setup={item.setup} punchline={item.punchline} />
+        ))}
+      </div>
     </div>
   )
 }
