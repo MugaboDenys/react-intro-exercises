bf5e10355dd265d9fc14f39eed0831c26c673bb8
MugaboDenys <mugabodan2020@gmail.com>
2023-03-23 11:54:51 +0200

    done add state

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [times, setTimes] = useState(0)
 
   const handleClick = () =>{
-    window.alert("Clicked!")
+    setTimes(prevTime => prevTime + 1)
   }
 
   const genLinks = () =>{
@@ -22,7 +24,8 @@ function App() {
           <a key={index} href={item.link} className={`px-2 underline underline-offset-4 ${!index == 0 && "border-indigo-600 border-l"} text-indigo-600 `} >{index == 0 ? "Home" : item.text}</a>
         ))}
       </div>
-      <button onClick={handleClick} className="py-2 px-4 border-2 mt-64 border-black rounded">Click Me</button>
+      <h2 className='mt-32'>Button has been clicked: {times} times </h2>
+      <button onClick={handleClick} className="py-2 px-4 border-2 mt-10 border-black rounded">Click Me</button>
     </div>
   )
 }
