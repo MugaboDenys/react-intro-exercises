f18c97d11f775d0fd79a2a9e3ab12b5fe30ffc5c
MugaboDenys <mugabodan2020@gmail.com>
2023-03-23 12:36:21 +0200

    added an array and mapped through it

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,6 @@ import './App.css'
 
 function App() {
 
-  const handleClick = () =>{
-    window.alert("Clicked!")
-  }
-
   const genLinks = () =>{
     let arr = []
     for(let i = 0; i< 10; i++){
@@ -15,6 +11,7 @@ function App() {
   }
 
   const array = genLinks()
+  const array1 = ['dog', 'cat', 'chicken', 'cow', 'sheep', 'horse']
   return (
     <div className="h-screen flex flex-col items-center ">
       <div>
@@ -22,7 +19,11 @@ function App() {
           <a key={index} href={item.link} className={`px-2 underline underline-offset-4 ${!index == 0 && "border-indigo-600 border-l"} text-indigo-600 `} >{index == 0 ? "Home" : item.text}</a>
         ))}
       </div>
-      <button onClick={handleClick} className="py-2 px-4 border-2 mt-64 border-black rounded">Click Me</button>
+      <ul className='mt-32 flex flex-col items-start'>
+          {array1.map(item=>(
+            <li key={item} className="list-disc">{item}</li>
+          ))}
+      </ul>
     </div>
   )
 }
