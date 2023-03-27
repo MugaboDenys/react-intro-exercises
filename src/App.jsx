ef3cb7a3720394f09816f55f790afdf80254889e
MugaboDenys <mugabodan2020@gmail.com>
2023-03-27 11:51:59 +0200

    card flip fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
+import { useState } from "react"
+import { useEffect } from "react"
 import Card from "./components/Card"
 import data from "./data"
 
 const App = () => {
+  const [count, setCount] = useState(0)
+  const [usersData, setUsersData] = useState([])
+  useEffect(()=>{
+    async function getData(){
+      try {
+        const res = await fetch("https://random-data-api.com/api/users/random_user?size=10")
+        const data = await res.json()
+        let array = []
+        data.forEach(user=> array.push({image: user.avatar, fname : user.first_name, lname : user.last_name, employment:user.employment}))
+        console.log(array);
+        setUsersData(array)
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    getData()
+  },[count])
 
-  const handleClick = () => window.alert("Clicked!")
-
+  const handleClick = () => setCount(prevCount=> prevCount+1)
 
   const genLinks = () =>{
     let arr = []
@@ -22,9 +40,12 @@ const App = () => {
           <a key={index} href={item.link} className={`px-2 underline underline-offset-4 ${!index == 0 && "border-indigo-600 border-l"} text-indigo-600 `} >{index == 0 ? "Home" : item.text}</a>
         ))}
       </div>
-      <div className="w-[40rem] p-5 h-72 mt-20 bg-[#dff8db] flex gap-5 items-center rounded-md">
-        {data.map(item=>(
-          <Card key={item.id} setup={item.setup} punchline={item.punchline} />
+      <div>
+        <button className="btn mt-20" onClick={handleClick}>Fetch Random</button>
+      </div>
+      <div className="w-[60rem] p-5 mt-20 bg-[#dff8db] grid grid-cols-2 justify-items-center gap-5 items-center rounded-md">
+        {usersData.map(item=>(
+          <Card key={item.id} {...item} />
         ))}
       </div>
     </div>
